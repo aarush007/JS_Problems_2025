5d2d922833cb170633a09d3bdaca9298cf7a94d3
Ashutosh Kumar <emailashu18@gmail.com>
2025-02-09 13:35:32 +0530

    update hof

--- Intermediate/hof.js
+++ Intermediate/hof.js
@@ -1,3 +1,39 @@
+// Approach 1
+
+const radius = [2, 4, 5, 8];
+
+const calculateArea = function (arr) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(Math.PI * arr[i] * arr[i]);
+  }
+  return output;
+};
+
+console.log(calculateArea(radius));
+
+const calculateCircumference = function (arr) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(2 * Math.PI * arr[i]);
+  }
+  return output;
+};
+
+console.log(calculateCircumference(radius));
+
+const calculateDiameter = function (arr) {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(2 * arr[i]);
+  }
+  return output;
+};
+
+console.log(calculateDiameter(radius));
+
+// Approach 2 (Optimized with calculate as Higher order function having area, circumference and diameter as callback function)
+
 const area = function (radius) {
   let result = Math.PI * radius * radius;
   return result.toFixed(2);
