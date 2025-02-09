c90c7c8d8d736577b79259af52b22be929fef408
Ashutosh Kumar <emailashu18@gmail.com>
2025-02-09 22:54:59 +0530

    update problem 9

--- Basics/problem9.js
+++ Basics/problem9.js
@@ -1,3 +1,5 @@
+// Approach 1
+
 function sumOfDigits(arr) {
   let sum = 0;
   for (let num of arr) {
@@ -11,3 +13,19 @@ function sumOfDigits(arr) {
 
 let numbers = [123, 45, 67, 890];
 sumOfDigits(numbers);
+
+// Approach 2
+
+const arr = [123, 67, 89, 345];
+
+const output = arr.reduce((acc, curr) => {
+  return (
+    acc +
+    curr
+      .toString()
+      .split("")
+      .reduce((sum, digit) => sum + Number(digit), 0)
+  );
+}, 0);
+
+console.log(output);
