90234e247d558e6c871e4ef25dcba5283a8fe77a
Ashutosh Kumar <emailashu18@gmail.com>
2025-03-27 10:28:50 +0530

    feat: word logger

---
+++ Intermediate/word_logger.js
@@ -0,0 +1,13 @@
+const sentence = "Hello Ashutosh, Have a good day!";
+const words = sentence.split(" ");
+const index = 0;
+
+const intervalId = setInterval(() => {
+  if (index < words.length) {
+    console.log(words[index]);
+    index++;
+  } else {
+    clearInterval(intervalId);
+    console.log(sentence);
+  }
+}, 1000);
