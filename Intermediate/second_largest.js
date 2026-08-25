bb331986fd146a1de8297be95be14f34e43df2a8
Ashutosh Kumar <emailashu18@gmail.com>
2026-08-25 23:11:42 +0530

    update second largest

--- Intermediate/second_largest.js
+++ Intermediate/second_largest.js
@@ -1,4 +1,22 @@
+// Approach 1
+
 const arr = [2, 3, 6, 8, 19, 78, 67, 43];
 
 const output = arr.sort((a, b) => b - a)[1];
 console.log(output);
+
+// Approach 2
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+    if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+    } else if (num > secondLargest && num !== largest) {
+        secondLargest = num;
+    }
+}
+
+console.log(secondLargest);
