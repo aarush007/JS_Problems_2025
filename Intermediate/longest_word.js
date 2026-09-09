0548ae0b4bc589a3e54c81fb2dc7e6aa5f5896cc
Ashutosh Kumar <emailashu18@gmail.com>
2026-09-09 19:15:11 +0530

    longest word

---
+++ Intermediate/longest_word.js
@@ -0,0 +1,13 @@
+function longestWord(sentence) {
+    const words = sentence.split(' ');
+    let longestWord = '';
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
+
+console.log(longestWord('my name is ashutosh kumar'));
